d917419dc3e4bcdbb96abe03d829a2c341a2281b
GimenezJorge <f289tqfgr8902>
2024-04-23 21:50:10 -0300

    prueba

--- practicaUno/Models/Bebida.cs
+++ practicaUno/Models/Bebida.cs
@@ -11,12 +11,37 @@ namespace practicaUno.Models
         public string nombre { get; set; }
         public int cantidad { get; set; }
 
+        //constructores mas completos:
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public int GetCantidad()
+        {
+            return cantidad;
+        }
+        public void SetCantidad(int cantidad)
+        {
+            this.cantidad= cantidad;
+        }
+
+        //constructor sin parametros:
+        public Bebida() { }
+        
+
         public Bebida(string nombre, int cantidad)
         {
             this.nombre = nombre;
             this.cantidad = cantidad;            
         }
 
+
+        //metodo
         public void tomarse(int cuantoBebio)
         {
             this.cantidad -= cuantoBebio;
--- practicaUno/Program.cs
+++ practicaUno/Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Bebida bebida= new Bebida("coquita", 500);
+        Bebida bebida= new Bebida();
         Bebida bebida2 = new Bebida("fanta", 500);
+        
         //bebida.nombre = "coquita";
         //bebida.cantidad = 500;
+
+        bebida.SetNombre("Coquita");
+        bebida.SetCantidad(500);
 
         bebida.tomarse(100);
         bebida2.tomarse(100);
